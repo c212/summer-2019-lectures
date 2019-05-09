6a22a2d41ddd9f512dded8297ecc7e8b8a757f6e
c212 <40395648+c212@users.noreply.github.com>
2019-05-09 12:50:41 -0400

    in which we add this
    what is this?

--- 0509/Fraction.java
+++ 0509/Fraction.java
@@ -2,22 +2,22 @@ public class Fraction {
     int numerator;
     int denominator;  
     public String toString() {
-        return numerator + "/" + denominator;    
+        return this.numerator + "/" + this.denominator;    
     }
     public Fraction add(Fraction other) {
         Fraction result = null; 
-        int n = numerator * other.denominator + denominator * other.numerator;
-        int d = denominator * other.denominator;
+        int n = this.numerator * other.denominator + this.denominator * other.numerator;
+        int d = this.denominator * other.denominator;
         result = new Fraction(n, d); 
         return result;
     }   
-    public Fraction(int a, int b) {
-        numerator = a;
-        denominator = b;
+    public Fraction(int numerator, int denominator) { // constructor
+        this.numerator = numerator;
+        this.denominator = denominator;
     }
     public static void main(String[] args) {
         Fraction a = new Fraction(1, 2); 
-        Fraction  b = new Fraction(1, 2);
+        Fraction b = new Fraction(1, 2);
         Fraction result = a.add(b);
         System.out.println( a + " + " + b + " = " + result );
         System.out.println( b + " + " + a + " = " + b.add(a) );
