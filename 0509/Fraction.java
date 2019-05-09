4bcecdae4b8a60fa5e61dc6cee6990b048d6a604
c212 <40395648+c212@users.noreply.github.com>
2019-05-09 12:10:30 -0400

    my first add
    does not do anything but also works

--- 0509/Fraction.java
+++ 0509/Fraction.java
@@ -1,8 +1,23 @@
 public class Fraction {
+    
     int numerator;
+    
     int denominator;
+    
     public Fraction(int a, int b) {
         numerator = a;
         denominator = b;
     }
+    
+    public static void main(String[] args) {
+        Fraction a = new Fraction(1, 2); 
+        Fraction  b = new Fraction(1, 3);
+        Fraction result = a.add(b);
+        System.out.println( a + " + " + b + " = " + result );
+    }
+    
+    public Fraction add(Fraction b ) {
+        return null; 
+    }
+    
 }
